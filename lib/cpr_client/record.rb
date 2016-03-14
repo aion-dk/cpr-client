61832ba1a1af540965b7eb961fd8e5859b957c07
Michael Andersen <michael.andersen.85@gmail.com>
2016-03-14 17:41:30 +0100

    Improve serialisation of Record
    XML doc is now parsed on Record initialisation, for improved
    serialization.

--- lib/cpr_client/record.rb
+++ lib/cpr_client/record.rb
@@ -1,13 +1,14 @@
 module CPRClient
   class Record
 
-    attr_reader :xml_doc
+    attr_reader :timestamp, :fields
 
     # Returns a new Response.
     #
     # @param xml_doc a Nokogiri::XML object
     def initialize(xml_doc)
-      @xml_doc = xml_doc
+      @timestamp = xml_doc.at_css("CprServiceHeader[r='STAMP']")['ts']
+      @fields    = extract_fields(xml_doc)
     end
 
     # Gets the value of a field with the given name.
@@ -15,8 +16,8 @@ module CPRClient
     # @param name the name of the target field.
     # @param value the name of the value attribute
     def get(name, value = 'v')
-      field = xml_doc.at_css("Praes[r='STAMPNR'] Field[r='#{name.to_s.upcase}']")
-      field && field[value.to_s.downcase]
+      field = @fields[name.to_s.upcase]
+      field[value.to_s.downcase] if field
     end
 
     alias_method :[], :get
@@ -48,24 +49,14 @@ module CPRClient
       '%s, %s %s' % values if values.all?
     end
 
-    # Returns a Hash with all values of all fields.
-    #
-    # The default value attribute is stored with field name as key.
-    # If any other attribute is found, it will have a key name of
-    # #{field_name}_#{attribute_name}.
-    #
-    # @return a Hash with string values
-    def values
-      fields = {}
-      xml_doc.css("Praes[r='STAMPNR'] Field").each do |field|
-        attrs = Hash[field.keys.zip(field.values)]
+    private
+
+    def extract_fields(xml_doc)
+      Hash[xml_doc.css("Praes[r='STAMPNR'] Field").reduce([]) { |a, f|
+        attrs = Hash[f.keys.zip(f.values)]
         key = attrs.delete('r')
-        fields[key] = attrs.delete('v')
-        attrs.each do |name, value|
-          fields["#{key}_#{name}"] = value
-        end
-      end
-      fields
+        attrs.empty? ? a : a << [key, attrs]
+      }]
     end
 
   end
